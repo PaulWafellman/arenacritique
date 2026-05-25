41694b3f44a14a7ecce0b7e0f9fda700b410a0c3
Paul Wafellman <paulwafellman@yahoo.com>
2026-05-25 15:06:23 +0200

    Ajout Dockerfile (oublié add .)

--- src/db/index.ts
+++ src/db/index.ts
@@ -12,17 +12,28 @@ if (!connectionString) {
 
 // Fonction pour initialiser la bonne instance de base de données
 const initDb = () => {
-  // Si on est en production ou que l'URL contient l'hôte de Neon
-  if (process.env.NODE_ENV === "production" || connectionString.includes("neon.tech")) {
+    // Si on est en production ou que l'URL contient l'hôte de Neon
+    // On regarde UNIQUEMENT si l'URL contient "neon.tech"
+  if (connectionString.includes("neon.tech")) {
     const client = neon(connectionString);
     return drizzleNeon({ client, schema });
-  } 
+  } else {
+    // Si c'est localhost (Docker), on utilise le client standard, 
+    // PEU IMPORTE si l'application est en mode dev ou production !
+    const pool = new pg.Pool({ connectionString });
+    return drizzle({ client: pool, schema });
+  }
   
-  // Sinon, on utilise le client standard pour Docker en local
-  const pool = new pg.Pool({
-    connectionString: connectionString,
-  });
-  return drizzle({ client: pool, schema });
+  //if (process.env.NODE_ENV === "production" || connectionString.includes("neon.tech")) {
+  //  const client = neon(connectionString);
+  //  return drizzleNeon({ client, schema });
+  //} 
+  //
+  //// Sinon, on utilise le client standard pour Docker en local
+  //const pool = new pg.Pool({
+  //  connectionString: connectionString,
+  //});
+  //return drizzle({ client: pool, schema });
 };
 
 export const db = initDb();
